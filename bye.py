9395ca2ac7acb4ebfb0cd0d04669e7043f90c5cb
Laiqa Abeba <cutlaiqa@ualberta.ca>
2026-07-08 16:02:58 -0600

    Add files via upload

---
+++ bye.py
@@ -0,0 +1,18 @@
+import csv
+import operator
+a = "Yes"
+
+with open('Pre_Study.csv') as file:
+  
+    
+ csv_reader = csv.reader(file, delimiter= ',')
+ next(csv_reader)
+ next(csv_reader)
+
+ 
+ sort = sorted(csv_reader, key = operator.itemgetter(14))
+
+ for eachline in sort:
+    if eachline[14] == a:
+      print(eachline[7], eachline[14], eachline[15])
+      
